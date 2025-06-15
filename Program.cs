6adceb00ae4c7d93ef53d3194805bdcc456e6b23
HejTyTheCoder <HejTy1000@seznam.cz>
2025-06-15 15:54:18 +0200

    feat: read URL from appsettings.json
    Add appsettings.json and modify Program.cs.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extension.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+string url = config["WeatherStationURL"];
+
+Console.WriteLine($"URL: {url}");
